1cdf78fa80fd03f098df1019ea34f94a9e99d1d1
paleaura01 <99763057+paleaura01@users.noreply.github.com>
2025-05-19 00:37:40 -0400

    yas

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 // svelte.config.js
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltePreprocess } from 'svelte-preprocess';
 import { resolve } from 'path';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   preprocess: sveltePreprocess({ typescript: true }),
   kit: {
-    adapter: adapter(),          // <-- no `runtime: 'edge'` unless all your code is edge-safe
+    adapter: adapter({ runtime: 'nodejs22.x' }),
     alias: {
       $lib: resolve('src/lib'),
       '$lib/server': resolve('src/lib/server')
